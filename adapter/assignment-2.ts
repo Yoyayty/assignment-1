7436e66776924780010bd2de589101f095fd4737
Aya <151888395+Yoyayty@users.noreply.github.com>
2024-09-19 03:32:34 +0000

    Revert "add mongodb usage"
    This reverts commit b4351aa7f195c0fa35f4ee7f66895df52179270a.

--- adapter/assignment-2.ts
+++ adapter/assignment-2.ts
@@ -1,7 +1,5 @@
 import assignment1 from "./assignment-1";
-import { connect } from '../src/db';
 
-import { ObjectId } from "mongodb";
 export type BookID = string;
 
 export interface Book {
@@ -13,25 +11,16 @@ export interface Book {
     image: string,
 };
 
-async function listBooks(filters?: Array<{ from?: number, to?: number }>): Promise<Book[]> {
+async function listBooks(filters?: Array<{from?: number, to?: number}>) : Promise<Book[]>{
     return assignment1.listBooks(filters);
 }
 
-
 async function createOrUpdateBook(book: Book): Promise<BookID> {
-    const db = await connect();
-    if (book.id) {
-        await db.collection('books').updateOne({ _id: new ObjectId(book.id) }, { $set: book });
-    } else {
-        const insertResult = await db.collection('books').insertOne(book);
-        book.id = insertResult.insertedId.toString(); // Set id as string
-    }
-    return book.id || "";
+    throw new Error("Todo")
 }
 
 async function removeBook(book: BookID): Promise<void> {
-    const db = await connect();
-    await db.collection('books').deleteOne({ _id: new ObjectId(book) });
+    throw new Error("Todo")
 }
 
 const assignment = "assignment-2";

84767a862b9aa0e24d89f389f43703c1d40987be
Aya <151888395+Yoyayty@users.noreply.github.com>
2024-11-04 01:50:07 +0000

    CHNAGE COMMANDS2

--- generate-openapi.sh
+++ generate-openapi.sh
@@ -1,3 +1,3 @@
 npx tsoa spec-and-routes
-npx @openapitools/openapi-generator-cli generate -i ./build/swagger.json -o ./client -g typescript-axios
+npx @openapitools/openapi-generator-cli generate -i ./build/swagger.json -o ./client -g typescript-fetch
 
